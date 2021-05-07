72691ac1de6d5a1c8d72bb7bb13c37aa13ac1739
thatguyish <ishmell.ramirez@gmail.com>
2021-05-06 20:52:49 -0400

    added database model, updated seed file & requir..

--- models.py
+++ models.py
@@ -1,5 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
+import datetime
 
+from sqlalchemy.orm import backref
 db = SQLAlchemy()
 
 def connect_db(app):
@@ -8,7 +10,7 @@ def connect_db(app):
 
 class User(db.Model):
     def __repr__(self):
-        return "self.user"
+        return f"{self.first_name}"
 
     __tablename__ = "users"
 
@@ -28,3 +30,22 @@ class User(db.Model):
     def delete_at_id(cls,d_id):
         cls.query.filter_by(id=d_id).delete()
         db.session.commit()
+
+    posts = db.relationship('Post', backref='user')
+
+class Post(db.Model):
+    def __repr__(self):
+        return f"{self.title}"
+
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer,primary_key=True,autoincrement=True)
+
+    title = db.Column(db.String(30),nullable=False)
+
+    content = db.Column(db.Text,nullable=False)
+
+    created_at = db.Column(db.DateTime,nullable=False,default=datetime.datetime.now)
+
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+
--- seed.py
+++ seed.py
@@ -1,11 +1,11 @@
 from app import app
-from models import db,User
+from models import Post, db,User
 
 db.drop_all()
 db.create_all()
 
 user_list = [(User(first_name="John",last_name="Ralfert")),(User(first_name="Albert",last_name="Syanasue")),(User(first_name="Rafael",last_name="Suso"))]
-
+post_list = [Post(title="first post",content="This is my first post",user_id=1),Post(title="second post",content="This is my second post",user_id=1),Post(title="chicken post",content="chicken is great",user_id=2)]
 db.session.add_all(user_list)
-
+db.session.add_all(post_list)
 db.session.commit()
